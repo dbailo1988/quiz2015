ce37af2b9c7904872e44a9c9bcfae5a75fd09aae
Daniel Bailo <dbailo1988@gmail.com>
2015-07-14 19:50:54 +0300

    Postgresql init

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 var quizController = require('../controllers/quiz_controller');
+
+router.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+})
 
 /* GET home page. */
 router.get('/', function(req, res) {
